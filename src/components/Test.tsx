0d966e86c0fd964342687c1d9a3e82ef6dbb67ca
jimmeryn <jimmeryn@gmail.com>
2022-02-05 23:40:06 +0100

    ramda + test

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,33 +1,48 @@
 import * as THREE from 'three';
 import React, { useRef, useState } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
+import { Canvas } from '@react-three/fiber';
+import { map } from 'ramda';
 
 const Box = (props: JSX.IntrinsicElements['mesh']) => {
   const mesh = useRef<THREE.Mesh>(null!);
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-  useFrame((state, delta) => (mesh.current.rotation.x += 0.01));
+  const [hovered, setHovered] = useState(false);
 
   return (
     <mesh
       {...props}
       ref={mesh}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
+      scale={1}
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
     >
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      <meshStandardMaterial color={hovered ? 'lightgreen' : 'lightblue'} />
     </mesh>
   );
 };
 
-export const Test = () => (
-  <Canvas>
-    <ambientLight />
-    <pointLight position={[10, 10, 10]} />
-    <Box position={[-1.2, 0, 0]} />
-    <Box position={[1.2, 0, 0]} />
-  </Canvas>
+const createBox = (position: [number, number, number] = [0, 0, 0]) => (
+  <Box position={position} />
 );
+
+export const Test = () => {
+  const c = 1.2;
+
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      {map(createBox)([
+        [-c, 0, 0],
+        [0, 0, 0],
+        [c, 0, 0],
+        [-c, c, 0],
+        [0, c, 0],
+        [c, c, 0],
+        [-c, -c, 0],
+        [0, -c, 0],
+        [c, -c, 0],
+      ])}
+    </Canvas>
+  );
+};
